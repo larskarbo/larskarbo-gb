bd81caa8ec3b0e55e4bd6806ba0c8ccb47dc966d
Lars Karbo <mail@larskarbo.no>
2023-08-14 21:44:20 +0200

    remove some gatsby stuff

--- web/gatsby-browser.js
+++
@@ -1,8 +0,0 @@
-// custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
-// normalize CSS across browsers
-// import "./src/normalize.css"
-// custom CSS styles
-import "./src/tailwind.css"
-import "./src/style.css"
